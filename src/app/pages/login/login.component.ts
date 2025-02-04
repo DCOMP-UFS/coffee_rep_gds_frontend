d9f960f2ad5f0f32bbfdc9ee1457a0e99f5e7b26
Guilherme Ilan <gui.tioze@gmail.com>
2025-02-03 23:44:32 -0300

    feat: create login page html + css

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  typeInput: string = "password"
 
+  tradeTypeInput(): void{
+    this.typeInput = this.typeInput === "password" ? "text" : "password";
+  }
 }
